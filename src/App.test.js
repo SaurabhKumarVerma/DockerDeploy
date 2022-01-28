72f7977a9de0facaf93a2ea3250702443772049e
Saurabh Kumar Verma <saurav88871kumar@hotmail.com>
2022-01-29 00:47:05 +0530

    New Test Case Added

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,5 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/USERNAME/i);
-  expect(linkElement).toBeInTheDocument();
+  screen.debug();
 });
